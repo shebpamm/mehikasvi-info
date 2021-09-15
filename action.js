b50b244a9b47c5bfe78138434b4cd257c9867eac
Erik Karsten <shebpamm@gmail.com>
2021-09-15 10:38:24 +0300

    Added icon, js deps, menu system and proper links

---
+++ action.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('#nav-menu').click(function(){
+
+        
+
+        var $selector = $('.header');
+        let closing = $selector.hasClass('open');
+        $selector
+        .data('oHeight', $selector.height())
+        .toggleClass('open')
+        .data('nHeight', $selector.height())
+        .toggleClass(closing ? 'open' : '')
+        .height($selector.data('oHeight'))
+        .velocity(
+            { height: $selector.data('nHeight')},
+            { duration: 400, 
+              easing: closing ? [0.11, 0, 0.5, 0] : [0.5, 1, 0.89, 1],
+              complete: () => 
+                {
+                    $selector
+                    .css('height', 'auto')
+                    .toggleClass(closing ? 'open' : '')
+                }
+            }
+        );
+    });
+});
